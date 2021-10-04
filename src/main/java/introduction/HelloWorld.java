0bcde6472489678e0b291903ac78cb7dab74d9fb
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2021-10-04 12:53:06 +0200

    log4j

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package introduction;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+        log.info("test");
     }
 }
